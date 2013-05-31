6de90b7706c694023e83af7ff1d0ccf05bd0bac0
Sophia Mao <sophiamao@umail.ucsb.edu>
2013-05-31 16:47:12 -0700

    added Instructions JFrame class

---
+++ src/FloodItInstructGui.java
@@ -0,0 +1,62 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/** 
+    Class where Pop-up JFrame for instructions (Flood it) is created.
+    @author Sophia Mao
+    @version 5/31/13 for proj 2, cs56, S13
+*/
+
+public class FloodItInstructGui{
+
+    //initialize and set JFrame visible and size
+    JFrame frame = new JFrame ("Rules");
+    frame.setVisible(true);
+    frame.setSize(400,400);
+
+    // create a new JTextArea to place on JFrame
+    JTextArea rules = new JTextArea(50, 50);
+ 
+    //From instructions.txt, read in the lines in the file and print instructions into the JTextArea declared above.
+    // use try catch block to check to make sure txt file exists.
+    try{
+	String line;
+	String fileName = "./src/instructions.txt"; //@@@ need to change when packaging happens.
+	
+	FileInputStream in = new FileInputStream(fileName);
+	BufferedReader br = new BufferedReader(new InputStream);
+	
+	//use a while loop to read all the text in txt file
+	while(( line = br.readLine()) != null){
+	    rules.append(line + "\n");
+	}
+    }catch(Exception e){
+	// print out current user directory and also exception method
+	System.out.println("specified file or directory does not exist in " + System.getProperty("user.dir") + "\n exception message" + e);
+    }
+   
+    //initialize aspects of JTextArea
+    rules.setEditable(false);
+    rules.setLineWrap(true);
+    rules.setWrapStyleWord(true);
+
+    //add JTextArea to a JScrollPane
+    JScrollPane scroller = new JScrollPane(rules);
+    scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+    scoller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+    //add JScrollPane to the frame
+    frame.add(scroller);
+    
+
+
+
+    }
+
+
+
+}
