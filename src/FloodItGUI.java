28dfb3cd0f08005d990e8b9e0a24555903f3c1c5
Kai-chieh Jann <kjann@umail.ucsb.edu>
2013-05-31 16:24:41 -0700

    added textarea and instruction button

--- src/FloodItGUI.java
+++ src/FloodItGUI.java
@@ -5,34 +5,53 @@ import javax.swing.*;
 
 public class FloodItGUI extends JFrame implements ActionListener{
 	
-		FloodItGUI(){
-		
-		
-		
-		}
-	
-		public static void main(String args[]){
-			
+			public void init(){
 			JFrame frame = new JFrame("Flood It!");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setSize(800,800);
-			frame.setVisible(true);
+			frame.setSize(1000,800);
+			
 			JButton buttonRed = new JButton("Red");
 			JButton buttonBlue = new JButton("Blue");
 			JButton buttonGreen = new JButton("Green");
 			JButton buttonYellow = new JButton("Yellow");
-
-			JPanel panel = new JPanel();
-	        panel.add(buttonRed);
-	        panel.add(buttonBlue);
-	        panel.add(buttonGreen);
-	        panel.add(buttonYellow);
-	        frame.getContentPane().add(BorderLayout.SOUTH,panel);
-
-
+	        JButton buttonInstruction = new JButton("Instruction");
+			
+	        JTextArea message = new JTextArea(50,20);
+	        
+			JPanel buttonPanel = new JPanel();
+			
+	        buttonPanel.add(buttonRed);
+	        buttonPanel.add(buttonBlue);
+	        buttonPanel.add(buttonGreen);
+	        buttonPanel.add(buttonYellow);
+	        
+	        frame.getContentPane().add(BorderLayout.SOUTH,buttonPanel);
+	                
+	        Container textContainer = new Container();
+	        textContainer.setLayout(new BoxLayout(textContainer, BoxLayout.Y_AXIS));
+	        textContainer.add(message);
+	        textContainer.add(buttonInstruction);
+	        
+	        frame.getContentPane().add(BorderLayout.EAST,textContainer);
+	        
+			frame.setVisible(true);
+		}
+		
+		public void run(){
+			init();
+		}
+		
+		
+		public static void main(String args[]){
+			
+			
+			
+	        
+	        FloodItGUI game = new FloodItGUI();
+	        game.run();
 			
 
-	}
+		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
